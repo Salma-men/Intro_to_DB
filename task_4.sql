dd2ebcfe4ba380c52685ccb29536e11b404598cd
Salma Mennani <men.salmaa@gmail.com>
2025-02-16 00:02:49 +0100

    Fix task_4.sql to properly describe the 'books' table

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,8 @@
-USE INFORMATION_SCHEMA;
-
 SELECT COLUMN_NAME, 
        COLUMN_TYPE, 
        IS_NULLABLE, 
        COLUMN_DEFAULT, 
        EXTRA
-FROM COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-  AND TABLE_NAME = 'books';
+FROM information_schema.columns
+WHERE table_schema = 'alx_book_store' 
+  AND table_name = 'books';
