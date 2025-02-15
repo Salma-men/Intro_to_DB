94fac2afc08eb1cf56d0b1f1ecadaf4d9d04dfcc
Salma Mennani <men.salmaa@gmail.com>
2025-02-16 00:13:20 +0100

    Fix task_4.sql to properly describe the 'books' table

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,11 @@
-SELECT COLUMN_NAME, 
-       COLUMN_TYPE, 
-       IS_NULLABLE, 
-       COLUMN_DEFAULT, 
-       EXTRA
-FROM information_schema.columns
-WHERE table_schema = 'alx_book_store' 
-  AND table_name = 'books';
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
